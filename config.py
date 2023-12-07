b7b367a4e05725dc91ae210aaf94f5c60e293a7b
Rogtiz <edwardraev@gmail.com>
2023-12-07 20:32:53 +0600

    Added config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+TOKEN = 'YOUR BOT TOKEN FROM BOT FATHER'
+
+
+# Local Database settings
+# host = 'localhost'
+# user = 'root'
+# database = 'quiz_bot'
+# password = 'root'
+
+# Host Database settings
+host = 'YOUR HOST'
+user = 'YOUR USER'
+database = 'YOUR DATABASE NAME'
+password = 'YOUR PASSWORD'
+
+
+API_KEY = 'YOUR KINOPOISK API KEY'
+API_URL = 'https://api.kinopoisk.dev/v1.4/movie/random?notNullFields=top250&isSeries=false'
